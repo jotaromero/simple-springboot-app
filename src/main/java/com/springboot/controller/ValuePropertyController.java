c655ba7f06ce49211a761e89663d1b41f58754c5
Alberto Romero <jarj@netcompany.com>
2021-04-21 16:18:05 +0200

    feature/jarj-modified-valueproperty-controller-v1

--- src/main/java/com/springboot/controller/ValuePropertyController.java
+++ src/main/java/com/springboot/controller/ValuePropertyController.java
@@ -1,26 +1,30 @@
 package com.springboot.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+@Slf4j
 @RestController
 @RequestMapping("/property")
 public class ValuePropertyController {
 
-	private Logger log = LoggerFactory.getLogger(getClass());
+  private final DateTimeFormatter DATE_TIME_FORMATTER =
+      DateTimeFormatter.ofPattern("HH:mm:ss.SSS dd/MM/yyyy");
+  private static final String MSG_TO_FORMAT =
+      "v1.0 / [%s] Value obtained from application.yml file is -> %s";
 
-	@Value("${configuration.property.value:default value}")
-	private String springProperty;
+  @Value("${configuration.property.value:default value}")
+  private String springProperty;
 
-	@GetMapping
-	public String getPropertyFromConfiguration() {
-		String message = String.format("Value obtained from application.yml file is -> %s", springProperty);
-		log.info("getPropertyFromConfiguration method called: {}", message);
-		return message;
-	}
+  @GetMapping
+  public String getPropertyFromConfiguration() {
+    String currentDateTime = LocalDateTime.now().format(DATE_TIME_FORMATTER);
+    String message = String.format(MSG_TO_FORMAT, currentDateTime, springProperty);
+    log.info("getPropertyFromConfiguration method called: {}", message);
+    return message;
+  }
 }
